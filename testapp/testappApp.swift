9a3eb0fe7f488157f3f50e9aad1c01de37d7cbd2
philipszdavido <kurtwanger40@gmail.com>
2023-11-21 10:47:03 +0100

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Chidume Nnamdi on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
